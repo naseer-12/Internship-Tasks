d675668fe09e88e199b054c4228d3b5ec42c9e98
naseer-12 <naseermohmand10@gmail.com>
2025-08-07 00:13:12 +0500

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+});
+
+
+
