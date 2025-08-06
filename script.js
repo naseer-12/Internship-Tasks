0ee76c539ceb042f91d2a1b2d7ea0f2593f702d5
naseer-12 <naseermohmand10@gmail.com>
2025-08-07 00:18:01 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.getElementById('toggleBtn');
-const sidebar = document.getElementById('sidebar');
-
-toggleBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('collapsed');
-});
-
-
-
